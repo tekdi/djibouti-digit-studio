f310d90c863e925efa516cc70a38043dc4d52a62
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2025-05-22 10:15:32 +0530

    remove debugger

--- frontend/web/micro-ui-internals/packages/modules/PublicServices/src/utils/index.js
+++ frontend/web/micro-ui-internals/packages/modules/PublicServices/src/utils/index.js
@@ -95,7 +95,6 @@ export const transformToApplicationPayload = (formData, configMap, service, tena
   const currentConfig = configMap?.ServiceConfiguration?.find((ob) => ob?.service === service);
 
   const serviceDetails = getServiceDetails(formData);
-  debugger;
   const applicants =
     formData.applicantDetails?.filter(Boolean)?.map((applicant, index) => ({
       type: "CITIZEN",
