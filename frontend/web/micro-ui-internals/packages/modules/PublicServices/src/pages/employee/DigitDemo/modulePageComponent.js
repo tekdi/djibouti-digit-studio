7116918f212f7fb31ef5e11398435dbce6bbd2f6
Akshay Gandhi <akshay.gandhi@tekditechnologies.com>
2025-06-12 11:10:33 +0530

    Revert "feat: display search card to counter employee"
    This reverts commit 601404c033dfc32fafcb18ff5cae4f937a304ced.

--- frontend/web/micro-ui-internals/packages/modules/PublicServices/src/pages/employee/DigitDemo/modulePageComponent.js
+++ frontend/web/micro-ui-internals/packages/modules/PublicServices/src/pages/employee/DigitDemo/modulePageComponent.js
@@ -30,7 +30,6 @@ const ModulePageComponent = () => {
   const userType = userDetails?.info?.type?.toLowerCase();
   const isCitizen = userDetails?.info?.roles?.some((role) => role.code === "CITIZEN");
   const isArchitect = userDetails?.info?.roles?.some((role) => role.code === "BPA_ARCHITECT");
-  const isCounterEmp = userDetails?.info?.roles?.some((role) => role.code === "COUNTER_EMPLOYEE");
 
   if (isLoading) {
     return <Loader />;
@@ -78,48 +77,8 @@ const ModulePageComponent = () => {
                 </Card>
               ))}
 
-            {isCounterEmp && (
-              <Card key={`${index}-inbox`} className="product-card module-card">
-                <div className="product-header inbox-header">
-                  <HeaderComponent className="product-title">{t("SEARCH_HEADER")}</HeaderComponent>
-                  <div className="product-icon">
-                    <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <rect width="56" height="56" rx="2" fill="#006769" />
-                      <g clip-path="url(#clip0_101_37716)">
-                        <path
-                          d="M41.3334 11.3333H14.6667C12.8334 11.3333 11.35 12.8333 11.35 14.6667L11.3334 44.6667L18 38H41.3334C43.1667 38 44.6667 36.5 44.6667 34.6667V14.6667C44.6667 12.8333 43.1667 11.3333 41.3334 11.3333ZM29.6667 26.3333H26.3334V16.3333H29.6667V26.3333ZM29.6667 33H26.3334V29.6667H29.6667V33Z"
-                          fill="white"
-                        />
-                      </g>
-                      <defs>
-                        <clipPath id="clip0_101_37716">
-                          <rect width="40" height="40" fill="white" transform="translate(8 8)" />
-                        </clipPath>
-                      </defs>
-                    </svg>
-                  </div>
-                </div>
-                <div className="">
-                  <Link
-                    className="link request-button"
-                    to={{
-                      pathname: `/${window.contextPath}/employee/publicservices/${product.module}/search`,
-                      state: {
-                        moduleData: data,
-                      },
-                    }}
-                  >
-                    {t(`${product?.module?.toUpperCase()}_SEARCH`)}
-                  </Link>
-                  <svg width="21" height="14" viewBox="0 0 21 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M13.5 0L12.09 1.41L16.67 6H0.5V8H16.67L12.08 12.59L13.5 14L20.5 7L13.5 0Z" fill="#006769" />
-                  </svg>
-                </div>
-              </Card>
-            )}
-
             {/* Inbox Card */}
-            {!isCitizen && !isCounterEmp && (
+            {!isCitizen && (
               <Card key={`${index}-inbox`} className="product-card module-card">
                 <div className="product-header inbox-header">
                   <HeaderComponent className="product-title">{t("INBOX_HEADING")}</HeaderComponent>
@@ -152,6 +111,14 @@ const ModulePageComponent = () => {
                     <path d="M13.5 0L12.09 1.41L16.67 6H0.5V8H16.67L12.08 12.59L13.5 14L20.5 7L13.5 0Z" fill="#006769" />
                   </svg>
                 </div>
+                {/* <Link className="link" to={{
+                pathname: `/${window.contextPath}/employee/publicservices/${product.module}/search`,
+                state: {
+                  moduleData:data
+                }
+              }}>
+                Search
+              </Link> */}
               </Card>
             )}
 
@@ -189,8 +156,17 @@ const ModulePageComponent = () => {
                     <path d="M13.5 0L12.09 1.41L16.67 6H0.5V8H16.67L12.08 12.59L13.5 14L20.5 7L13.5 0Z" fill="#006769" />
                   </svg>
                 </div>
+                {/* <Link className="link" to={{
+                pathname: `/${window.contextPath}/employee/publicservices/${product.module}/search`,
+                state: {
+                  moduleData:data
+                }
+              }}>
+                Search
+              </Link> */}
               </Card>
             )}
+
             <style jsx>{`
               .module-card {
                 width: 100%;
