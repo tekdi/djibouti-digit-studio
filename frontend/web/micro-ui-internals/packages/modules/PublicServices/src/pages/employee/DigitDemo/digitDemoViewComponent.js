040869f2061be97a6035034c2616be9571e8b88a
sonaligarud <sonali.garud@tekditechnologies.com>
2025-07-21 17:29:01 +0530

    fix:design office detailing shows empty in application review form

--- frontend/web/micro-ui-internals/packages/modules/PublicServices/src/pages/employee/DigitDemo/digitDemoViewComponent.js
+++ frontend/web/micro-ui-internals/packages/modules/PublicServices/src/pages/employee/DigitDemo/digitDemoViewComponent.js
@@ -40,6 +40,9 @@ const DigitDemoViewComponent = () => {
       applicationNumber: queryStrings?.applicationNumber,
       tenantId: tenantId,
     },
+    config: {
+      cacheTime: 0,
+    },
   };
   const { isLoading, data } = Digit.Hooks.useCustomAPIHook(request);
   let response = data ? data?.Application?.[0] : {};
@@ -92,7 +95,7 @@ const DigitDemoViewComponent = () => {
     documents: response?.documents || [],
     serviceDetails: {
       landInfo: response?.serviceDetails?.landandProjectDesignDetails?.[0] || {},
-      designOffice: response?.serviceDetails?.designOfficeDetailing?.[0] || {},
+      designOffice: response?.serviceDetails?.designOfficeDetailing || [],
     },
   };
 
